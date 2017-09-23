968f3e4515c41264f0ae1e553bfac01632dc9289
shaudai <shaudai0402@hotmail.com>
2017-09-23 22:31:08 +0800

    change the value in a variable (variable mutation)

--- script.js
+++ script.js
@@ -28,3 +28,8 @@ job = 'teacher';
 isMarried = false;
 
 console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
+
+age = 'thirty six';
+job = 'driver';
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
