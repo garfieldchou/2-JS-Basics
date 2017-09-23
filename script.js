971cf6683ec7581f5b8271d76b66e79df0d88b88
shaudai <shaudai0402@hotmail.com>
2017-09-23 22:39:46 +0800

    use alert() to display message on window

--- script.js
+++ script.js
@@ -34,5 +34,8 @@ job = 'driver';
 
 console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
 
-var lastName = prompt('What is the last name?');
-console.log( lastName );
+// var lastName = prompt('What is the last name?');
+// console.log( lastName );
+
+
+alert(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
