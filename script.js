b610981de10bf5d546fc2cc9fd217d6977186a50
shaudai <shaudai0402@hotmail.com>
2017-09-24 17:34:15 +0800

    simple function to calculate age

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('Hello World in script.js');
+// Lecture: Functions
+function calculateAge(yearOfBirth) {
+    var age = 2017 - yearOfBirth;
+    return age;
+}
+
+var ageJohn = calculateAge(1990);
+console.log(ageJohn);
