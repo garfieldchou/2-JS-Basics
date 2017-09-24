11f199a463096ade7ddda4bfb6e9302985f34ed3
shaudai <shaudai0402@hotmail.com>
2017-09-24 13:48:03 +0800

    switch statements

--- script.js
+++ script.js
@@ -112,7 +112,23 @@ if (age < 20) {
     console.log('John is a man.');
 }
 
+var job = 'teacher';
 
+job = prompt('What does john do?');
+
+switch (job) {
+    case 'teacher':
+        console.log('John teaches kids.');
+        break;
+    case 'driver':
+        console.log('John drives a cab in Lisbon.');
+        break;
+    case 'cop':
+        console.log('John helps fight crime.');
+        break;
+    default:
+        console.log('John does something else.');
+}
 
 
 
