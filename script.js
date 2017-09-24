0dbdc679a5f433f120abc497efda96b81883fae9
shaudai <shaudai0402@hotmail.com>
2017-09-24 17:12:00 +0800

    coding challenge 1- draw

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Hello World in script.js');
+var heightJohn = 180;
+var heightMike = 165;
+var ageJohn = 26;
+var ageMike = 29;
+
+var scoreJohn = heightJohn + 5 * ageJohn;
+var scoreMike = heightMike + 5 * ageMike;
+
+if (scoreJohn > scoreMike) {
+    console.log('John wins the game with ' + scoreJohn + ' points!');
+} else if (scoreJohn < scoreMike) {
+    console.log('Mike wins the game with ' + scoreMike + ' points!');
+} else if (scoreJohn === scoreMike) {
+    console.log('There is a draw.');
+}
