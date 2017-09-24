0f4c48a470fb373b16e68532c62596b0604de2c7
shaudai <shaudai0402@hotmail.com>
2017-09-24 17:49:40 +0800

    function call another function without return

--- script.js
+++ script.js
@@ -7,4 +7,19 @@ function calculateAge(yearOfBirth) {
 var ageJohn = calculateAge(1990);
 var ageMike = calculateAge(1969);
 var ageMary = calculateAge(1948);
-console.log(ageMary);
+
+
+function yearsUntilRetirement(name, year) {
+    var age = calculateAge(year);
+    var retirement = 65 - age;
+    
+    if (retirement >= 0) {
+        console.log(name + ' retires in ' + retirement + ' years.');
+    } else {
+        console.log(name + ' is already retired.');
+    }
+}
+
+yearsUntilRetirement('John', 1990);
+yearsUntilRetirement('Mike', 1969);
+yearsUntilRetirement('Mary', 1948);
