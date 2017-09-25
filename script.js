2a01a5433780b8c40aef70d9941eecfd846e6efb
shaudai <shaudai0402@hotmail.com>
2017-09-25 22:52:55 +0800

    object literal about john

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hello World in script.js');
+// Lecture: Objects
+
+var john = {
+    name: 'John',
+    lastName: 'Smith',
+    yearOfBirth: 1990,
+    job: 'teacher',
+    isMarried: false
+};
+
+console.log(john);
