393d4d99a62738243cb42acd0ed828b4380c6f66
shaudai <shaudai0402@hotmail.com>
2017-09-26 06:44:49 +0800

    function expression in an object

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Hello World in script.js');
+// Lecture: Objects and methods
+
+var john = {
+    name: 'John',
+    lastName: 'Smith',
+    yearOfBirth: 1990,
+    job: 'teacher',
+    isMarried: false,
+    family: ['Jane', 'Mark', 'Bob'],
+    calculateAge: function(yearOfBirth) {
+        return 2017 - yearOfBirth;
+    }
+};
+
+console.log(john);
