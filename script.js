f4147a6df48bad119a3d9beb08d8de159a7e9cb3
garfield_chou <garfield_chou@asus.com>
2017-09-26 13:00:13 +0800

    create an age property in the method

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Lecture: Objects and methods
 
+// v1.0
+/*
 var john = {
     name: 'John',
     lastName: 'Smith',
@@ -17,4 +19,21 @@ console.log(john.calculateAge());
 var age = john.calculateAge();
 john.age = age;
 
+console.log(john);
+*/
+
+// v2.0
+var john = {
+    name: 'John',
+    lastName: 'Smith',
+    yearOfBirth: 1990,
+    job: 'teacher',
+    isMarried: false,
+    family: ['Jane', 'Mark', 'Bob'],
+    calculateAge: function() {
+        this.age = 2017 - this.yearOfBirth;
+    }
+};
+
+john.calculateAge();
 console.log(john);
