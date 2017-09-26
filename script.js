0ff9c22f7151d4e6ad3772aadf80478db0cec5d2
shaudai <shaudai0402@hotmail.com>
2017-09-26 22:13:34 +0800

    a simple loop

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Hello World in script.js');
+// Lecture: Loops
+
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+/*
+0, true, print 0, update i to 1
+1, true, print 1, update i to 2
+.
+.
+.
+9, true, print 9, update i to 10
+10, FALSE, end loop!
+*/
